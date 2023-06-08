408469e178dd141d6195e1a77e9d815cbc26097f
aleixnit <nitjac210@gmail.com>
2023-06-08 11:49:29 +0200

    iteration 1 completed

--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,19 @@
 // Your code here
+    export default function FoodBox(food) {
+return (
+    <div>
+  <p>NAME</p>
+
+  <img src="IMAGE" />
+
+  <p>Calories: CALORIES</p>
+  <p>Servings SERVINGS</p>
+
+  <p>
+    <b>Total Calories: SERVINGS * CALORIES </b> kcal
+  </p>
+
+  <button>Delete</button>
+</div>
+);
+    }
